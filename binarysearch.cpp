c1960c2d17fdf6a700c7d0c6275129b9752a1835
Aditya Mishra <adityam0338@gmail.com>
2022-10-08 14:13:39 +0530

    added comments

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 bool binarysearch(vector<int>& arr, int elem){
     int left=0;
-    sort(arr.begin(), arr.end());
+    sort(arr.begin(), arr.end()); //since binary search works only in sorted array
     int right= arr.size()-1;
     while(left<=right){
-        int pivot= left + (right-1)/2;
-        if(arr[pivot]==elem)
+        int pivot= left + (right-1)/2; //finding out the center element
+        if(arr[pivot]==elem)   
             return true;
-        if(arr[pivot]>elem)
+        if(arr[pivot]>elem)  //if center element is greater than the element to search, the element must be in first half, so right changes to pivot-1
             right=pivot-1;
         else
             left= pivot+1;
@@ -20,14 +20,14 @@ bool binarysearch(vector<int>& arr, int elem){
 int main(){
     vector<int> arr;
     int n;
-    cin>>n;
+    cin>>n; //size of array
     int x=0;
     for(int i=0;i<n;i++){
         cin>>x;
         arr.push_back(x);
     }
     int elem=0;
-    cin>>elem;
+    cin>>elem; //the element to search
     bool ispresent= binarysearch(arr, elem);
     if(ispresent)
         cout<<"Element Found";
